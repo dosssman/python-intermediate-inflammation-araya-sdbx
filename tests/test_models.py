db5d07f07cf4e2f39a0289972ced2db065c0fdad
Rousslan F.J. Dossa <dosssman@hotmail.fr>
2023-10-04 06:18:03 +0000

    Re-tested up to Github CI

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,13 +1,14 @@
 """Tests for statistics functions within the Model layer."""
 
+import pytest
+
 import numpy as np
 import numpy.testing as npt
 
+from inflammation.models import daily_mean
 
 def test_daily_mean_zeros():
     """Test that mean function works for an array of zeros."""
-    from inflammation.models import daily_mean
-
     test_input = np.array([[0, 0],
                            [0, 0],
                            [0, 0]])
@@ -19,8 +20,6 @@ def test_daily_mean_zeros():
 
 def test_daily_mean_integers():
     """Test that mean function works for an array of positive integers."""
-    from inflammation.models import daily_mean
-
     test_input = np.array([[1, 2],
                            [3, 4],
                            [5, 6]])
@@ -29,3 +28,22 @@ def test_daily_mean_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_mean(test_input), test_result)
 
+def test_daily_max_integers():
+    """Test that max function works for an array of positive integers."""
+    test_input = np.array([[1, 2],
+                        [3, 4],
+                        [5, 6]])
+    test_result = np.array([3, 4])
+
+    npt.assert_array_equal(daily_mean(test_input), test_result)
+
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([ [0, 0], [0, 0], [0, 0] ], [0, 0]),
+        ([ [1, 2], [3, 4], [5, 6] ], [3, 4]),
+    ])
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
+    
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
